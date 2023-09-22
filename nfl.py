a7fdfc88e2c8c6ca2dc37cfe83073a3e141dbcf4
andrew <andrew@andrewconl.in>
2023-09-22 12:14:11 +0100

    [22/09/23] initial commit
    v0.1
    two classes defined, season and week. current functionlity allows you to
    pull information about all the games in a season from ESPN, and store in
    a season object

---
+++ nfl.py
@@ -0,0 +1,79 @@
+""" 
+A set of classes for getting data about an NFL season
+
+Author: Andrew Conlin
+Last updated: 22nd Sep 2023 
+Version: 0.1
+"""
+
+import requests
+import json
+
+# Season object, containing all of the information on a current season
+# Properties:
+# - year: year of the season, needed to initialise the object
+# - weeks: array containing information on each week. This is stored in 'week' objects
+# Methods:
+# - getSchedule(weekStart, weekEnd): get the schedule for the current year (from ESPN). 
+#       weekStart and weekEnd define the range of weeks to generate a schedule for. 
+#       Will overwrite the exisiting schedule if called again.
+class season:
+    def __init__(self, year):
+        self.year = year
+        self.weeks = []
+
+    def __str__(self):
+        return f"A season object for the {self.year} NFL season"
+
+    def getSchedule(self, weekStart, weekEnd):
+        self.weeks = []
+        for weekN in range(weekStart,weekEnd+1):
+            print(f"Fetching data for week {weekN}")
+            url = f"https://cdn.espn.com/core/nfl/schedule?xhr=1&year={self.year}&week={weekN}"
+            
+            print(f"--->    Parsing data for week {weekN}")
+            data = requests.get(url).json()
+            # print(data['content']['schedule'])
+            schedule = data['content']['schedule']
+
+            currentWeek = week(weekN)
+            currentWeek.setGames(schedule)
+
+            self.weeks.append(currentWeek)
+
+# Week object, containing all information on a current week
+# Properties:
+# - weekN: week number
+# Methods
+# - setGames: set the games for this week 
+class week:
+    def __init__(self, weekN):
+        self.weekN = weekN
+        self.games = []
+        # self.rawData = data
+        # self.jsonData = self.rawData.json()
+        # self.parsedData = json.load(self.jsonData)
+
+    def __str__(self):
+        return f"Week {self.weekN}"
+
+    def setGames(self,schedule):
+        dates = self.extractKeys(schedule)
+        for date in dates:
+            for game in schedule[date]['games']:
+                self.games.append(game)
+
+    # Internal methods
+    def extractKeys(self,dict):
+        return list(dict.keys())
+
+
+
+
+
+# test = season('2023')
+# print(test)
+
+# test.getSchedule(3,3)
+# print(test.weeks[0].weekN)
+# print(test.weeks[0].games[0]['shortName'])
