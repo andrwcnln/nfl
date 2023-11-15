ad2bfbe14d08de1db5598a5b28f5739a679a7465
andrew <andrew@andrewconl.in>
2023-11-15 14:54:10 +0000

    [15/11/23] Removing testGames method as API values are changing frequently

--- tests/testSeason.py
+++ tests/testSeason.py
@@ -1,44 +1,45 @@
 import pytest
 import requests
 
-@pytest.mark.parametrize('year',['2020','2021','2022'])
+
+@pytest.mark.parametrize("year", ["2020", "2021", "2022"])
 class testSeason:
     @pytest.fixture(autouse=True)
-    def setup(self,loadCache,loadObject):
+    def setup(self, loadCache, loadObject):
         self.cache = loadCache
         self.obj = loadObject
 
-    def testGames(self,year):
-        expected = self.cache[year]
-        actual = self.obj[year]
+        # def testGames(self,year):
+        # expected = self.cache[year]
+        # actual = self.obj[year]
 
-        expectedCounter = 0
-        for weekActual in actual.weeks:
-            for gameActual in weekActual.games:
-                assert gameActual == expected['games'][expectedCounter]
-                expectedCounter += 1
+        # expectedCounter = 0
+        # for weekActual in actual.weeks:
+        # for gameActual in weekActual.games:
+        # assert gameActual == expected['games'][expectedCounter]
+        # expectedCounter += 1
 
-    def testWinners(self,year):
+    def testWinners(self, year):
         expected = self.cache[year]
         actual = self.obj[year]
 
         expectedCounter = 0
         for weekActual in actual.weeks:
             for winnerActual in weekActual.winners:
-                assert winnerActual == expected['winners'][expectedCounter]
+                assert winnerActual == expected["winners"][expectedCounter]
                 expectedCounter += 1
 
-    def testScores(self,year):
+    def testScores(self, year):
         expected = self.cache[year]
         actual = self.obj[year]
 
         expectedCounter = 0
         for weekActual in actual.weeks:
             for scoreActual in weekActual.scores:
-                assert scoreActual == expected['scores'][expectedCounter]
+                assert scoreActual == expected["scores"][expectedCounter]
                 expectedCounter += 1
 
-    def testAPIResponse(self,year):
+    def testAPIResponse(self, year):
         url = f"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates={year}&seasontype=2&week=1"
         resp = requests.get(url)
         assert resp.status_code == 200
